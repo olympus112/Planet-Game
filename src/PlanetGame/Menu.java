accbf3fee4907fb4dcb2b48826ef74d9ba750fb6
Olympus112 <matthivandersanden@gmail.com>
2016-11-07 22:48:41 +0100

    Added classes

---
+++ src/PlanetGame/Menu.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Menu extends BasicGameState{
+
+
+
+    @Override public int getID() {
+        return Main.MENU;
+    }
+
+    @Override public void init(GameContainer container, StateBasedGame game) throws SlickException {
+
+    }
+
+    @Override public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
+
+    }
+
+    @Override public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
+
+    }
+}
